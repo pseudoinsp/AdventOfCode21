bf0683943379e724335919c1dd5851530206f885
Endre Berki <endre.berki@instructure.com>
2021-12-01 13:37:31 +0100

    Day 1 part 2

--- Day1/day1.rb
+++ Day1/day1.rb
@@ -1,5 +1,13 @@
 measurements = []
 File.foreach("#{__dir__}/input.txt") { |line| measurements << line.to_i }
 
+#part1
 increases = measurements.select.with_index { |x, i| i > 0 && measurements[i] > measurements[i-1]}.count
 puts "Increase in depth levels: #{increases}"
+
+#part2
+sums = measurements.map.with_index do |x, i|
+    i >= 2 ? measurements[i-2..i].sum : 9999
+end
+sum_increases = sums.select.with_index { |x, i| i > 0 && sums[i] > sums[i-1]}.count
+puts "Increase in depth levels of 3 depth combinations: #{sum_increases}"
