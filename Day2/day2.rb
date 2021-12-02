b7b5495e6677e5c775c72dc41e23b15b2093bdb1
Endre Berki <endre.berki@instructure.com>
2021-12-02 10:48:14 +0100

    Day 2 part 2

--- Day2/day2.rb
+++ Day2/day2.rb
@@ -1,6 +1,7 @@
 instructions = []
 File.foreach("#{__dir__}/input.txt") { |line| instructions << line }
 
+#part 1
 position = { x: 0, y: 0 }
 instructions.each do |instr|
     if instr =~ /up (\d)/
@@ -9,6 +10,23 @@ instructions.each do |instr|
         position[:y] += $1.to_i
     else
         position[:x] += instr[/forward (\d)/, 1].to_i
+    end
+end
+
+puts "Final position coordinates multiplied #{position[:x] * position[:y]}"
+
+#part 2
+position = { x: 0, y: 0, aim: 0 }
+
+instructions.each do |instr|
+    if instr =~ /up (\d)/
+        position[:aim] -= $1.to_i
+    elsif instr =~ /down (\d)/
+        position[:aim] += $1.to_i
+    else
+        change = instr[/forward (\d)/, 1].to_i
+        position[:x] += change
+        position[:y] += position[:aim] * change
     end
 end
 
