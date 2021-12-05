54221f556aaeffe3b97a88c36a8fe7dbee6e6ec3
pseudoinsp <gonosztaviszorny@gmail.com>
2021-12-05 15:34:24 +0100

    Add Rubocop config, fix day5

--- day5.rb
+++ day5.rb
@@ -1,6 +1,6 @@
 def read_file
   lines = File.readlines('inputs/day5.txt')
-  lines.map {|line| /(?<x1>\d+),(?<y1>\d+) -> (?<x2>\d+),(?<y2>\d+)/.match(line).named_captures.to_h { |k, v| [k.to_sym, v.to_i] } }
+  lines.map { |line| /(?<x1>\d+),(?<y1>\d+) -> (?<x2>\d+),(?<y2>\d+)/.match(line).named_captures.to_h { |k, v| [k.to_sym, v.to_i] } }
 end
 
 def create_layout(coordinates, count_diagonal:)
@@ -19,18 +19,15 @@ def create_layout(coordinates, count_diagonal:)
         key = "#{x};#{coordinate[:y1]}"
         layout[key] += 1
       end
-    else
-      # part 2
-      if count_diagonal
-        lower_x, higher_x = [coordinate[:x1], coordinate[:x2]].minmax
-        lower_y, higher_y = [coordinate[:y1], coordinate[:y2]].minmax
-        y_for_lower_x = lower_x == coordinate[:x1] ? coordinate[:y1] : coordinate[:y2]
+    elsif count_diagonal
+      lower_x, higher_x = [coordinate[:x1], coordinate[:x2]].minmax
+      lower_y, higher_y = [coordinate[:y1], coordinate[:y2]].minmax
+      y_for_lower_x = lower_x == coordinate[:x1] ? coordinate[:y1] : coordinate[:y2]
 
-        (lower_x..higher_x).each_with_index do |x, i|
-          y = y_for_lower_x == lower_y ? lower_y + i : higher_y - i
-          key = "#{lower_x + i};#{y}"
-          layout[key] += 1
-        end
+      (lower_x..higher_x).each_with_index do |_, i|
+        y = y_for_lower_x == lower_y ? lower_y + i : higher_y - i
+        key = "#{lower_x + i};#{y}"
+        layout[key] += 1
       end
     end
   end
@@ -40,12 +37,12 @@ end
 
 def part1(input)
   layout = create_layout(input, count_diagonal: false)
-  puts "Overlapping points: #{layout.count { |k, v| v > 1 }}"
+  puts "Overlapping points: #{layout.count { |_k, v| v > 1 }}"
 end
 
 def part2(input)
   layout = create_layout(input, count_diagonal: true)
-  puts "Overlapping points with diagonal: #{layout.count { |k, v| v > 1 }}"
+  puts "Overlapping points with diagonal: #{layout.count { |_k, v| v > 1 }}"
 end
 
 part1(read_file)
