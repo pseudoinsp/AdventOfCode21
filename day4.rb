eeb275d7a1b37b0c224e85d0451b2139e0419e9e
pseudoinsp <gonosztaviszorny@gmail.com>
2021-12-05 15:46:32 +0100

    Day 4 rubocop fix

--- day4.rb
+++ day4.rb
@@ -1,48 +1,47 @@
 def read_file
   lines = File.readlines('inputs/day4.txt')
-  
+
   drawn = lines.first.split(',').map(&:to_i)
 
   tables_data = lines[2..-1].reject{ |line| line.strip.empty? }
-  tables = tables_data.each_slice(5).to_a.map.with_index{|data, i| BingoTable.new(i + 1, data) }
-  return drawn, tables
+  tables = tables_data.each_slice(5).to_a.map.with_index { |data, i| BingoTable.new(i + 1, data) }
+  [drawn, tables]
 end
 
 class BingoTable
-
   attr_accessor :rows, :columns, :board_number
 
   def initialize(board_number, input)
     @board_number = board_number
-    row_i = input.map{|row_input| row_input.split.map(&:to_i) }
+    row_i = input.map { |row_input| row_input.split.map(&:to_i) }
     @rows = row_i.map do |row|
-      h = Hash.new { |h, k| h[k] = false }
+      h = Hash.new { |_h, k| h[k] = false }
       row.each { |num| h[num] }
       h
     end
-    
+
     col_i = row_i.transpose
     @columns = col_i.map do |col|
-      h = Hash.new { |h, k| h[k] = false }
+      h = Hash.new { |_h, k| h[k] = false }
       col.each { |num| h[num] }
       h
     end
   end
 
-  def has_bingo_with_number?(number)
-    row_hit = rows.find{ |row| row.has_key? number }
+  def bingo_with_number?(number)
+    row_hit = rows.find { |row| row.key? number }
     return false if row_hit.nil?
 
-    col_hit = columns.find{ |col| col.has_key? number }
+    col_hit = columns.find { |col| col.key? number }
 
     row_hit[number] = true
     col_hit[number] = true
 
-    row_hit.all? { |k, v| v == true } || col_hit.all? { |k, v| v == true } 
+    row_hit.all? { |_k, v| v == true } || col_hit.all? { |_k, v| v == true }
   end
 
   def unmarked_numbers_sum
-    rows.map { |row| row.filter { |k, v| v == false }.keys }.flatten.sum
+    rows.map { |row| row.filter { |_k, v| v == false }.keys }.flatten.sum
   end
 end
 
@@ -50,10 +49,10 @@ def part1(input)
   drawn, tables = input
 
   catch :lolreally do
-    while true
+    loop do
       drawn.each do |drawn_number|
         tables.each do |table|
-          if table.has_bingo_with_number?(drawn_number)
+          if table.bingo_with_number?(drawn_number)
             puts "Final score: #{drawn_number * table.unmarked_numbers_sum}"
             throw :lolreally
           end
@@ -71,23 +70,22 @@ def part2(input)
   last_drawn_number = -1
   last_table_unmarked = -1
 
-  while remaining_tables.length > 0
+  until remaining_tables.empty?
     drawn.each do |drawn_number|
       remaining_tables.each do |table_id, table|
-        if table.has_bingo_with_number?(drawn_number)
-          if remaining_tables.length == 1
-            last_drawn_number = drawn_number
-            last_table_unmarked = table.unmarked_numbers_sum
-          end
-          remaining_tables.delete(table_id)
+        next unless table.bingo_with_number?(drawn_number)
+
+        if remaining_tables.length == 1
+          last_drawn_number = drawn_number
+          last_table_unmarked = table.unmarked_numbers_sum
         end
+        remaining_tables.delete(table_id)
       end
     end
   end
 
   puts "Final score: #{last_table_unmarked * last_drawn_number}"
-
 end
 
-# part1(read_file)
+part1(read_file)
 part2(read_file)
