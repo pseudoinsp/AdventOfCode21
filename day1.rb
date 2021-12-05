bff9ca54d17f230232b6e71f03d2543d58de2c60
pseudoinsp <gonosztaviszorny@gmail.com>
2021-12-05 15:54:56 +0100

    Rubocop fix day 1 and 2

--- day1.rb
+++ day1.rb
@@ -1,11 +1,11 @@
 measurements = []
-File.foreach("inputs/day1.txt") { |line| measurements << line.to_i }
+File.foreach('inputs/day1.txt') { |line| measurements << line.to_i }
 
-#part1
-increases = measurements.select.with_index { |x, i| i > 0 && measurements[i] > measurements[i-1]}.count
+# part1
+increases = measurements.select.with_index { |_x, i| i > 0 && measurements[i] > measurements[i - 1] }.count
 puts "Increase in depth levels: #{increases}"
 
-#part2
-sums = measurements.map.with_index { |x, i| i >= 2 ? measurements[i-2..i].sum : 9999 }
-sum_increases = sums.select.with_index { |x, i| i > 0 && sums[i] > sums[i-1]}.count
+# part2
+sums = measurements.map.with_index { |_x, i| i >= 2 ? measurements[i - 2..i].sum : 9999 }
+sum_increases = sums.select.with_index { |_x, i| i > 0 && sums[i] > sums[i - 1] }.count
 puts "Increase in depth levels of 3 depth combinations: #{sum_increases}"
--- day2.rb
+++ day2.rb
@@ -1,33 +1,33 @@
 instructions = []
-File.foreach("inputs/day2.txt") { |line| instructions << line }
+File.foreach('inputs/day2.txt') { |line| instructions << line }
 
-#part 1
+# part 1
 position = { x: 0, y: 0 }
 instructions.each do |instr|
-    if instr =~ /up (\d)/
-        position[:y] -= $1.to_i
-    elsif instr =~ /down (\d)/
-        position[:y] += $1.to_i
-    else
-        position[:x] += instr[/forward (\d)/, 1].to_i
-    end
+ if instr =~ /up (\d)/
+   position[:y] -= Regexp.last_match.to_i
+ elsif instr =~ /down (\d)/
+   position[:y] += Regexp.last_match.to_i
+ else
+   position[:x] += instr[/forward (\d)/, 1].to_i
+ end
 end
 
 puts "Final position coordinates multiplied #{position[:x] * position[:y]}"
 
-#part 2
+# part 2
 position = { x: 0, y: 0, aim: 0 }
 
 instructions.each do |instr|
-    if instr =~ /up (\d)/
-        position[:aim] -= $1.to_i
-    elsif instr =~ /down (\d)/
-        position[:aim] += $1.to_i
-    else
-        change = instr[/forward (\d)/, 1].to_i
-        position[:x] += change
-        position[:y] += position[:aim] * change
-    end
+  if instr =~ /up (\d)/
+    position[:aim] -= Regexp.last_match.to_i
+  elsif instr =~ /down (\d)/
+    position[:aim] += Regexp.last_match.to_i
+  else
+    change = instr[/forward (\d)/, 1].to_i
+    position[:x] += change
+    position[:y] += position[:aim] * change
+  end
 end
 
 puts "Final position coordinates multiplied #{position[:x] * position[:y]}"
