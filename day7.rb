11d43316225792fe95fad7dabd3ae5d02c423f11
Endre Berki <endre.berki@instructure.com>
2021-12-07 12:10:09 +0100

    Day 7 part 2

--- day7.rb
+++ day7.rb
@@ -2,13 +2,13 @@ def read_file
   File.read('inputs/day7.txt').split(',').map(&:to_i)
 end
 
-def get_optimal_position(positions)
+def get_optimal_position(positions, &fuel_cost_calc)
   min, max = positions.minmax
 
   opt = { pos: -1, cost: 1 << 64 }
 
   (min..max).each do |poss_pos|
-    cost = positions.map {|pos| (poss_pos - pos).abs }.sum
+    cost = positions.map { |pos| fuel_cost_calc.call(poss_pos, pos) }.sum
 
     if cost < opt[:cost]
       opt = { pos: poss_pos, cost: cost }
@@ -19,14 +19,17 @@ def get_optimal_position(positions)
 end
 
 def part1(input)
-  opt = get_optimal_position(input)
-  puts opt[:cost]
+  opt = get_optimal_position(input) { |possible_position, position| (possible_position - position).abs } 
+  p opt[:cost]
 end
   
 def part2(input)
+  opt = get_optimal_position(input) do |possible_position, position| 
+    difference = (possible_position - position).abs 
+    (1..difference).sum
+  end
+  p opt[:cost]
 end
   
 part1(read_file)
 part2(read_file)
-
-  
