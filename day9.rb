86ea47c6aaf33fe6031d9271a8ea8b4000e04e29
pseudoinsp <gonosztaviszorny@gmail.com>
2021-12-09 23:47:17 +0100

    Day 9 part 2

--- day9.rb
+++ day9.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 def read_file
   flowmap = []
   File.foreach('inputs/day9.txt') { |line| flowmap << line.strip.split('').map(&:to_i) }
@@ -28,10 +30,48 @@ def part1(flowmap)
   end
 
   p risk_level
- 
 end
 
-def part2(input)
+$basin_sum = 0
+def get_basin_size(field, x, y)
+  visited = Set.new
+  $basin_sum = 0
+
+  traverse(field, x, y, visited)
+  $basin_sum
+end
+
+def traverse(field, x, y, visited)
+  return if visited.include?("#{x};#{y}")
+
+  visited.add("#{x};#{y}")
+  $basin_sum += 1
+
+  dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+  dirs.each do |dir|
+    next unless x + dir[0] >= 0 && x + dir[0] < field.length && y + dir[1] >= 0 && y + dir[1] < field[0].length
+
+    x1 = x + dir[0]
+    y1 = y + dir[1]
+    neighbour = field[x1][y1]
+    traverse(field, x1, y1, visited) if neighbour != 9
+  end
+end
+
+def part2(flowmap)
+  basin_sizes = []
+
+  flowmap.each.with_index do |line, x|
+    line.each.with_index do |point, y|
+      if is_low_point?(flowmap, x, y)
+        size = get_basin_size(flowmap, x, y)
+        basin_sizes << size
+      end
+    end
+  end
+
+  three_highest_size_basins = basin_sizes.sort.reverse.first(3).reduce(&:*)
+  p three_highest_size_basins
 end
 
 part1(read_file)
